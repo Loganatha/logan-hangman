bb1071748979ddff602976f26c3000ca47f49f2f
Loganatha <kraykarisma@gmail.com>
2017-05-27 01:04:35 -0500

    code cleanup and documentation, removing debugging console logging

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,20 @@
 window.onload = function() {
   /*
   * adds replaceAll functionality to javacript string primitive
+  *
+  * normal replace stops after the first instance it finds, handing in a RegExp
+  * with the search parameter will search over the entire string
+  *
+  * See javascript regular expressions for more information:
+  * https://regex101.com
   */
   String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
+    var target = this; // create a copy of the string that invoked the function
+    // run a regex search and replace over the entire string
     return target.replace(new RegExp(search, 'g'), replacement);
   };
 
+  // array of alphabet letters
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
   var categories; // Array of topics
@@ -32,16 +40,30 @@ window.onload = function() {
   * scaffold alphabet list
   */
   var buttons = function() {
+    // get button container by id from the page (index.html@L14)
     myButtons = document.getElementById('buttons');
+    // create an html unordered list element to contain the alphabet buttons
     letters = document.createElement('ul');
+    // set the html id property of the letters container
+    letters.id = 'alphabet';
+    // append the newly created letters container inside the myButtons container
+    myButtons.appendChild(letters);
 
+    // begin with an index of 0 (first array member), and iterate to the limit
+    // of alphabet.length, incrementing the index by one for each pass in the
+    // loop
     for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
+      // create an html list item (li) to add to the letters list
       list = document.createElement('li');
+      // set the html class property of the new list item
       list.className = 'letter';
+      // set the innerHTML (text content) of the list item to the item at
+      // the current alphabet array index position
       list.innerHTML = alphabet[i];
+      // call the function which attaches the onclick event handler to the
+      // list item
       check();
-      myButtons.appendChild(letters);
+      // append the list item to the letters container
       letters.appendChild(list);
     }
   }
@@ -50,13 +72,17 @@ window.onload = function() {
   * select and display category
   */
   var selectCat = function() {
+    // set the category names according to their index position in the
+    // categories list
     if (chosenCategory === categories[0]) {
-      var catName = "ANIMALS";
+      var catName = "ANIMALS"; // e.g. if index 0, set the catName to ANIMALS
     } else if (chosenCategory === categories[1]) {
       var catName = "PLACES";
     } else if (chosenCategory === categories[2]) {
       var catName = "MEMES";
     }
+    // now concatenate a string including the category name set above and push
+    // that string to the categoryName element
     catagoryName.innerHTML = "The category is " + catName;
   }
 
@@ -64,25 +90,34 @@ window.onload = function() {
   * scaffold guesses list
   */
   result = function() {
+    // get the hold element from the page
     wordHolder = document.getElementById('hold');
+    // create the element for displaying correct guesses
     correct = document.createElement('ul');
+    // another method of setting the elements html id parameter
+    correct.setAttribute('id', 'my-word');
+    // add the guess-holder element to the wordHolder container
+    wordHolder.appendChild(correct);
 
+    // loop over the word
     for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
+      // create a list item for the guess blank
       guess = document.createElement('li');
+      // set the classname of the guess blank
       guess.setAttribute('class', 'guess');
+      // if the current letter is a dash, treat it as a space
       if (word[i] === "-") {
-        guess.innerHTML = "-";
+        guess.innerHTML = "-"; // set the text content of the guess blank
         space += 1;
-      } else if (word[i] === "'") {
+      } else if (word[i] === "'") { // else if apostrophe, treat as such
         guess.innerHTML = "'";
         apostrophe += 1;
-      } else {
+      } else { // otherwise, render an underscore (letter blank)
         guess.innerHTML = "_";
       }
-
+      // add the guess blank to the guesses array
       guesses.push(guess);
-      wordHolder.appendChild(correct);
+      // also, append our guess to the guess-holder element
       correct.appendChild(guess);
     }
   }
@@ -91,14 +126,18 @@ window.onload = function() {
   * display tries
   */
   comments = function() {
+    // by default, show the number of tries remaining
     showLives.innerHTML = "You have " + tries + " tries remaining";
+    // if the tries counter reaches 0, you fail
     if (tries < 1) {
+      document.getElementsByTagName('body')[0].className = 'fail';
       showLives.innerHTML = "You Fail";
     }
-    for (var i = 0; i < guesses.length; i++) {
-      if (counter + space + apostrophe === guesses.length) {
-        showLives.innerHTML = "Success!";
-      }
+    // if the number of correctly guessed letters, plus spaces, plus apostrophes
+    // equals the length of the word, the answer is correct
+    if (counter + space + apostrophe === guesses.length) {
+      document.getElementsByTagName('body')[0].className = 'success';
+      showLives.innerHTML = "Success!";
     }
   }
 
@@ -106,32 +145,43 @@ window.onload = function() {
   * draw man
   */
   var animate = function() {
+    // cast tries to a new variable for this function
     var drawMe = tries;
+    // call the drawArray func at the index position represented by the current
+    // number of tries
     drawArray[drawMe]();
   }
 
   /*
   * hangman components
   */
+
+  // draw the canvas
   canvas = function() {
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
     context.strokeStyle = "#ffffff";
-    context.lineWidth = 2;
+    context.lineWidth = 4;
   };
 
+  // draw the head
   head = function() {
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
+    // sweep an arc through 360 degrees to make a circle
     context.arc(60, 25, 10, 0, Math.PI * 2, true);
     context.stroke();
   }
 
+  // generic function from drawing a line
   draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    // move drawing pointer to x and y position
     context.moveTo($pathFromx, $pathFromy);
+    // set ending coordinate position of line
     context.lineTo($pathTox, $pathToy);
+    // draw the line!
     context.stroke();
   }
 
@@ -171,6 +221,9 @@ window.onload = function() {
     draw(60, 70, 20, 100);
   };
 
+  // we have 10 tries and 10 hangman parts
+  // the array is in reverse order because we call it with the number of
+  // remaining tries at line 152, above
   drawArray = [
     rightLeg,
     leftLeg,
@@ -185,26 +238,39 @@ window.onload = function() {
   ];
 
   /*
-  * OnClick event Function
+  * OnClick event Function for alphabet list items
   */
   check = function() {
+    // when this list item is clicked...
     list.onclick = function() {
+      // get the letter from the inner html of the element
       var guess = (this.innerHTML);
+      // set the html class to change the styling with css
       this.setAttribute("class", "active");
+      // remove this click handler to prevent further interaction
       this.onclick = null;
+      // loop to the length of the word
       for (var i = 0; i < word.length; i++) {
+        // if this guessed letter exists in the word
         if (word[i] === guess) {
+          // set the content of the guess blank on the page to this letter
           guesses[i].innerHTML = guess;
+          // increment the correct guesses counter
           counter += 1;
         }
       }
+      // get the position of this letter in the word
       var j = (word.indexOf(guess));
-      if (j === -1) {
+      if (j === -1) { // a position of -1 means the letter wasn't found
+        // decrement the tries counter
         tries -= 1;
+        // call comments to update the comments on the page, showing status
         comments();
+        // call animate to draw the hangman part corresponding to the number of
+        // tries remaining
         animate();
-      } else {
-        comments();
+      } else { // if the letter WAS found in the word
+        comments(); // just fire the comments function
       }
     }
   }
@@ -213,6 +279,7 @@ window.onload = function() {
   * play function
   */
   play = function() {
+    // categories array, of answer arrays, numbered starting at 0
     categories = [
       [
         "blobfish",
@@ -231,28 +298,39 @@ window.onload = function() {
       ]
     ];
 
+    // pseudo-randomly choose a category index position
     chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    // within the chosen category, select an answer
     word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    // replace all the spaces with dashes for display purposes
     word = word.replaceAll(/\s/g, "-");
+    // render the alphabet buttons w/ the function at line 42, above
     buttons();
 
+    // re-initialize the game variables, initially declared starting at line 20
     guesses = [];
     tries = 10;
     counter = 0;
     space = 0;
     apostrophe = 0;
+
+    // call the functions that render the rest of the page components
     result();
     comments();
     selectCat();
     canvas();
   }
 
+  // placing our call the play() at the outer-most level insite the
+  // window.onload function will fire play() at line 201 when the page loads
   play();
 
   /*
   * hint feature onClick function
   */
   hint.onclick = function() {
+    // an array of hints corresponding to the answers in the categories at line
+    // 203, above
     hints = [
       [
         "lazy sea creature",
@@ -270,23 +348,31 @@ window.onload = function() {
         "lives in a something, under something-something"
       ]
     ];
-
+    // reverse the replacement of blank spaces with hyphens
     revWord = word.replaceAll("-", " ");
-    console.log(revWord);
+    // get the arrax index position of the chosen category and answer word
     var catagoryIndex = categories.indexOf(chosenCategory);
     var hintIndex = chosenCategory.indexOf(revWord);
-    console.log("category: " + catagoryIndex + " | word: " + hintIndex);
+    // render the clue corresponding to the answer in play
     showClue.innerHTML = "Clue: " + hints[catagoryIndex][hintIndex];
   };
 
   /*
   * reset onClick function
   */
+  // when the button with the id parameter value of 'reset' throws a click
   document.getElementById('reset').onclick = function() {
+    // remove the success and failure classes from the body element
+    document.getElementsByTagName('body')[0].classList.remove("success");
+    document.getElementsByTagName('body')[0].classList.remove("fail");
+    // remove the answer word and letter choices
     correct.parentNode.removeChild(correct);
     letters.parentNode.removeChild(letters);
+    // blow away the clue content
     showClue.innerHTML = "";
+    // clear the hangman canvas
     context.clearRect(0, 0, 400, 400);
+    // fire it all back up!
     play();
   }
 }
